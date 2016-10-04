f01c71a5ae30e0e81f060e875357bd55fdb8ae5d
FunkyMicrowave <snefill@gmail.com>
2016-10-04 09:24:17 +0000

    Add files via upload

---
+++ error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>DC Error</title>
+    <link href="styles/journey.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="wrapper">
+    <h1 style="color:#EEE">
+        <?php
+        echo "Því miður kom upp villa, prófaðu aftur síðar";
+        ?>
+    </h1>
+    </div>
+</body>
+</html>
